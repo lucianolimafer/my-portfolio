07842863e04175778f37bfb1d85ae5660eed84e5
Luciano Lima Fer <lucianolimaf3r@gmail.com>
2021-11-25 12:41:20 -0300

    :rocket: Feat - Add a animated section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react";
+import Section from "../components/section";
 
 const Page = () => {
   const bgColor = useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')
@@ -27,6 +28,13 @@ const Page = () => {
         />
       </Box>
       </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Workssss</p>
+      </Section>
     </Container>
   );
 }
