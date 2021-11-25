fcef0b359651b19ec0016726838b896981bae385
Luciano Lima Fer <lucianolimaf3r@gmail.com>
2021-11-25 14:01:56 -0300

    :rocket: Feat - add the bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import {
 import Section from "../components/section";
 import Paragraph from "../components/paragraph";
 import { ChevronRightIcon } from '@chakra-ui/icons';
+import { BioSection, BioYear } from '../components/bio';
 
 const Page = () => {
   const bgColor = useColorModeValue('whiteAlpha.500', 'whiteAlpha.200');
@@ -62,6 +63,55 @@ const Page = () => {
             </Button>
           </NextLink>
         </Box>
+      </Section>
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1993</BioYear>
+          Born in Valença do Piauí - PI, Brazil
+        </BioSection>
+        <BioSection>
+          <BioYear>2001 - 2009</BioYear>
+          Completed the college in Valença do Piauí - PI, Brazil
+        </BioSection>
+        <BioSection>
+          <BioYear>2010 - 2012</BioYear>
+          Completed the High School in Valença do Piauí - PI, Brazil
+        </BioSection>
+        <BioSection>
+          <BioYear>2013 - 2015</BioYear>
+          Started the Graduation in Analisys and Development of Systems, in Institute of Science and Technolgy from Picos - PI, Brazil
+        </BioSection>
+        <BioSection>
+          <BioYear>2014 - 2015</BioYear>
+          Started working like a manager on the department of public administration from files in Prefeitura Municipal Valença do Piauí - PI, Brazil
+        </BioSection>
+        <BioSection>
+          <BioYear>2016 - 2016</BioYear>
+          Started working like a social media in a agency from publicity Global Monster, Teresina - PI, Brazil.
+        </BioSection>
+        <BioSection>
+          <BioYear>2017 - 2018</BioYear>
+          Started working as a electronic technician on the TRF 4ª Region, on Porto Alegre - RS, Brazil
+        </BioSection>
+        <BioSection>
+          <BioYear>2018 - 2020</BioYear>
+          Started working as a electronic technician on the TRF 4ª Region, on Porto Alegre - RS, Brazil
+        </BioSection>
+        <BioSection>
+          <BioYear>2018 - 2020</BioYear>
+          Started working as a front-end developer freelancer, on Porto Alegre - RS, Brazil
+        </BioSection>
+        <BioSection>
+          <BioYear>2021 - 2021</BioYear>
+          Started working as a front-end developer in a Software house, the company Rockapps, from Rio de Janeiro, Brazil.
+        </BioSection>
+        <BioSection>
+          <BioYear>2021 - actually</BioYear>
+          Working as a front-end ande mobile developer in a outsourcing squad to Globalsys company, from Vitoria - ES, Brazil, and the biggest club of wine from the world, wine.com.br.
+        </BioSection>
       </Section>
     </Container>
   );
