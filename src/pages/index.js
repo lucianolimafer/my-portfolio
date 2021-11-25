ceffc1cda0bb1297a3028ef4661e15656b5e36a5
Luciano Lima Fer <lucianolimaf3r@gmail.com>
2021-11-25 13:19:00 -0300

    :rocket: Feat - Add paragraph component to bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,21 @@
-import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react";
+import NextLink from 'next/link';
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+  Link,
+  Button
+} from "@chakra-ui/react";
 import Section from "../components/section";
+import Paragraph from "../components/paragraph";
+import { ChevronRightIcon } from '@chakra-ui/icons';
 
 const Page = () => {
-  const bgColor = useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')
+  const bgColor = useColorModeValue('whiteAlpha.500', 'whiteAlpha.200');
+  const arrowComponent = <ChevronRightIcon />;
+
   return (
     <Container>
       <Box borderRadius="lg" bg={bgColor} p={3} mb={6} align="center">
@@ -16,24 +29,39 @@ const Page = () => {
           <p>Mobile developer (js / ts)</p>
         </Box>
         <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
-        <Image
-          borderColor="whiteAlpha.800"
-          borderWidth={3}
-          borderStyle="solid"
-          maxWidth="25%"
-          display="inline-block"
-          borderRadius="full"
-          src="https://avatars.githubusercontent.com/u/69359504?v=4"
-          alt="Profile Image"
-        />
-      </Box>
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={3}
+            borderStyle="solid"
+            maxWidth="25%"
+            display="inline-block"
+            borderRadius="full"
+            src="https://avatars.githubusercontent.com/u/69359504?v=4"
+            alt="Profile Image"
+          />
+        </Box>
       </Box>
 
       <Section delay={0.1}>
         <Heading as="h3" variant="section-title">
-          Work
+          Bio
         </Heading>
-        <p>Workssss</p>
+        <Paragraph>
+          I'm a mobile developer with over two years of development experience and improving my skills with Javascript and Typescript to build high-performance and high complexity hybrid applications (android and ios) using the react-native framework, my
+          {' '}
+          <NextLink href="/works/inkdrop">
+            <Link>Linkedin</Link>
+          </NextLink>
+          .
+        </Paragraph>
+
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={arrowComponent} colorScheme="orange">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
       </Section>
     </Container>
   );
