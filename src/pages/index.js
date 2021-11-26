dcbadf973f5463c21835ad70d91af4fbef3e8e37
Luciano Lima Fer <lucianolimaf3r@gmail.com>
2021-11-26 15:12:51 -0300

    :rocket: Feat - Add content

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import {
   Link,
   Button
 } from "@chakra-ui/react";
+import Layout from '../components/layouts/article';
 import Section from "../components/section";
 import Paragraph from "../components/paragraph";
 import { ChevronRightIcon } from '@chakra-ui/icons';
@@ -18,102 +19,104 @@ const Page = () => {
   const arrowComponent = <ChevronRightIcon />;
 
   return (
-    <Container>
-      <Box borderRadius="lg" bg={bgColor} p={3} mb={6} align="center">
-        Hi, I'm a front-end developer focused on building mobile and web apps.
-      </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Luciano Lima Fer
+    <Layout>
+      <Container>
+        <Box borderRadius="lg" bg={bgColor} p={3} mb={6} align="center">
+          Hi, I'm a front-end developer focused on building mobile and web apps.
+        </Box>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Luciano Lima Fer
+            </Heading>
+            <p>Mobile developer (js / ts)</p>
+          </Box>
+          <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={3}
+              borderStyle="solid"
+              maxWidth="25%"
+              display="inline-block"
+              borderRadius="full"
+              src="https://avatars.githubusercontent.com/u/69359504?v=4"
+              alt="Profile Image"
+            />
+          </Box>
+        </Box>
+
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            About me
           </Heading>
-          <p>Mobile developer (js / ts)</p>
-        </Box>
-        <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={3}
-            borderStyle="solid"
-            maxWidth="25%"
-            display="inline-block"
-            borderRadius="full"
-            src="https://avatars.githubusercontent.com/u/69359504?v=4"
-            alt="Profile Image"
-          />
-        </Box>
-      </Box>
+          <Paragraph>
+            I'm a mobile developer with over two years of development experience and improving my skills with Javascript and Typescript to build high-performance and high complexity hybrid applications (android and ios) using the react-native framework, my
+            {' '}
+            <NextLink href="/works/inkdrop">
+              <Link>Linkedin</Link>
+            </NextLink>
+            .
+          </Paragraph>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          About me
-        </Heading>
-        <Paragraph>
-          I'm a mobile developer with over two years of development experience and improving my skills with Javascript and Typescript to build high-performance and high complexity hybrid applications (android and ios) using the react-native framework, my
-          {' '}
-          <NextLink href="/works/inkdrop">
-            <Link>Linkedin</Link>
-          </NextLink>
-          .
-        </Paragraph>
-
-        <Box align="center" my={4}>
-          <NextLink href="/works">
-            <Button rightIcon={arrowComponent} colorScheme="orange">
-              My portfolio
-            </Button>
-          </NextLink>
-        </Box>
-      </Section>
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1993</BioYear>
-          Born in Valença do Piauí - PI, Brazil
-        </BioSection>
-        <BioSection>
-          <BioYear>2001 - 2009</BioYear>
-          Completed the college in Valença do Piauí - PI, Brazil
-        </BioSection>
-        <BioSection>
-          <BioYear>2010 - 2012</BioYear>
-          Completed the High School in Valença do Piauí - PI, Brazil
-        </BioSection>
-        <BioSection>
-          <BioYear>2013 - 2015</BioYear>
-          Started the Graduation in Analisys and Development of Systems, in Institute of Science and Technolgy from Picos - PI, Brazil
-        </BioSection>
-        <BioSection>
-          <BioYear>2014 - 2015</BioYear>
-          Started working like a manager on the department of public administration from files in Prefeitura Municipal Valença do Piauí - PI, Brazil
-        </BioSection>
-        <BioSection>
-          <BioYear>2016 - 2016</BioYear>
-          Started working like a social media in a agency from publicity Global Monster, Teresina - PI, Brazil.
-        </BioSection>
-        <BioSection>
-          <BioYear>2017 - 2018</BioYear>
-          Started working as a electronic technician on the TRF 4ª Region, on Porto Alegre - RS, Brazil
-        </BioSection>
-        <BioSection>
-          <BioYear>2018 - 2020</BioYear>
-          Started working as a electronic technician on the TRF 4ª Region, on Porto Alegre - RS, Brazil
-        </BioSection>
-        <BioSection>
-          <BioYear>2018 - 2020</BioYear>
-          Started working as a front-end developer freelancer, on Porto Alegre - RS, Brazil
-        </BioSection>
-        <BioSection>
-          <BioYear>2021 - 2021</BioYear>
-          Started working as a front-end developer in a Software house, the company Rockapps, from Rio de Janeiro, Brazil.
-        </BioSection>
-        <BioSection>
-          <BioYear>2021 - actually</BioYear>
-          Working as a front-end ande mobile developer in a outsourcing squad to Globalsys company, from Vitoria - ES, Brazil, and the biggest club of wine from the world, wine.com.br.
-        </BioSection>
-      </Section>
-    </Container>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={arrowComponent} colorScheme="orange">
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1993</BioYear>
+            Born in Valença do Piauí - PI, Brazil
+          </BioSection>
+          <BioSection>
+            <BioYear>2001 - 2009</BioYear>
+            Completed the college in Valença do Piauí - PI, Brazil
+          </BioSection>
+          <BioSection>
+            <BioYear>2010 - 2012</BioYear>
+            Completed the High School in Valença do Piauí - PI, Brazil
+          </BioSection>
+          <BioSection>
+            <BioYear>2013 - 2015</BioYear>
+            Started the Graduation in Analisys and Development of Systems, in Institute of Science and Technolgy from Picos - PI, Brazil
+          </BioSection>
+          <BioSection>
+            <BioYear>2014 - 2015</BioYear>
+            Started working like a manager on the department of public administration from files in Prefeitura Municipal Valença do Piauí - PI, Brazil
+          </BioSection>
+          <BioSection>
+            <BioYear>2016 - 2016</BioYear>
+            Started working like a social media in a agency from publicity Global Monster, Teresina - PI, Brazil.
+          </BioSection>
+          <BioSection>
+            <BioYear>2017 - 2018</BioYear>
+            Started working as a electronic technician on the TRF 4ª Region, on Porto Alegre - RS, Brazil
+          </BioSection>
+          <BioSection>
+            <BioYear>2018 - 2020</BioYear>
+            Started working as a electronic technician on the PD7 Technolgy, on Porto Alegre - RS, Brazil
+          </BioSection>
+          <BioSection>
+            <BioYear>2018 - 2020</BioYear>
+            Started working as a front-end developer freelancer, on Porto Alegre - RS, Brazil
+          </BioSection>
+          <BioSection>
+            <BioYear>2021 - 2021</BioYear>
+            Started working as a front-end developer in a Software house, the company Rockapps, from Rio de Janeiro, Brazil.
+          </BioSection>
+          <BioSection>
+            <BioYear>2021 - actually</BioYear>
+            Working as a front-end ande mobile developer in a outsourcing squad to Globalsys company, from Vitoria - ES, Brazil, and the biggest club of wine from the world, wine.com.br.
+          </BioSection>
+        </Section>
+      </Container>
+    </Layout>
   );
 }
 
---
+++ src/pages/works.js
@@ -0,0 +1,43 @@
+import {
+  Container,
+  Heading,
+  SimpleGrid,
+  Divider
+} from '@chakra-ui/react';
+import Section from '../components/Section';
+import {
+  GridItem,
+  WorkGridItem,
+  GridItemStyle
+} from '../components/grid-item';
+import Layout from '../components/layouts/article';
+import thumbLifepet from '../../public/images/works/inkdrop_01.webp';
+import thumbSatelite from '../../public/images/works/satelite_01.png';
+
+const Works = () => {
+  return (
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay={0.2}>
+            <WorkGridItem id="lifepet" title="Lifepet Partners" thumbnail={thumbLifepet}>
+              Mobile app for pet owners to track their pets' health and behavior.
+            </WorkGridItem>
+          </Section>
+
+          <Section delay={0.4}>
+            <WorkGridItem id="satelite" title="Satelite" thumbnail={thumbSatelite}>
+              A hybrid app (android and ios) built to manage video calls person to person or live for multiple users.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Works;
