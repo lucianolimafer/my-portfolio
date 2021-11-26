d8603fcf4db85d3c97bced2557b05b11ed23303e
Luciano Lima Fer <lucianolimaf3r@gmail.com>
2021-11-26 17:54:00 -0300

    :rocket: Feat  - help props to read text

--- src/components/bio.js
+++ src/components/bio.js
@@ -4,11 +4,25 @@ import styled from '@emotion/styled';
 export const BioSection = styled(Box)`
   padding-left: 3.4em;
   text-indent: -3.4em;
+
+  &:hover {
+    text-decoration: underline;
+    color: orange;
+  }
+
+  cursor: context-menu;
 `;
 
 export const BioYear = styled.span`
   font-weight: bold;
   margin-right: 1em;
+
+  &:hover {
+    text-decoration: underline;
+    color: orange;
+  }
+
+  cursor: context-menu;
 `;
 
 
